a5f506a7dae489ace9b2d5be5e77712a7554ae90
JoseANG3L <agelgzman@hotmail.com>
2025-12-29 23:55:09 -0600

    Anuncios Banner

--- src/pages/Peliculas.jsx
+++ src/pages/Peliculas.jsx
@@ -1,11 +1,14 @@
 import MovieCard from "../components/MovieCard";
 import MovieSection from "../components/MovieSection";
 import { PELICULAS } from "../data/movies";
+import AdSenseBanner from '../components/AdSenseBanner';
 
 function Peliculas() {
   return (
     <div className="pt-20 md:pt-24">
       <MovieSection title="Todas las Peliculas" movies={PELICULAS} layout="grid" enableFilters={true} />
+      {/* --- ANUNCIO 1 --- */}
+      <AdSenseBanner slot="1234567890" />
     </div>
   );
 }
